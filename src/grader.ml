8cffb3831146ab2e36301007c7ff3ea8628d03fc
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2014-09-30 16:30:21 -0400

    Print terms and types.

--- src/grader.ml
+++ src/grader.ml
@@ -1,7 +1,23 @@
 open Coqlib
+open Global
+open Libnames
+open Pp
+open Printer
+open Util
+
+let find_reference (path : string list) (id : string) : global_reference option =
+  try
+    Some (find_reference "" path id)
+  with
+  | Anomaly (_,_) -> None
 
 let () = Mltop.add_known_plugin (fun () ->
   Printf.printf "Grader plugin successfully loaded\n\n";
-  let r = find_reference "Couldn't find it..." ["Coq";"Lists"] "List" in
-  Printf.printf "Did it"
+  let r = find_reference ["Coq";"Lists";"List"] "app" in
+  match r with
+  | Some r ->
+    pp (pr_global r);
+    pp (brk (0,0));
+    pp (pr_constr (type_of_global r))
+  | None   -> Printf.printf "Couldn't find anything"
 ) "grader"
