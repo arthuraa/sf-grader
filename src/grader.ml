4107f14b5b969bd45c5447738eb9de3e0e7f60db
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2014-09-30 19:17:50 -0400

    Test comparing two definitions.

--- src/grader.ml
+++ src/grader.ml
@@ -1,23 +1,37 @@
+open Reductionops
 open Coqlib
+open Environ
 open Global
 open Libnames
 open Pp
 open Printer
+open Term
 open Util
 
-let find_reference (path : string list) (id : string) : global_reference option =
+let is_conv (t1 : constr) (t2 : constr) : bool =
+  is_conv empty_env Evd.empty t1 t2
+
+let ofind_reference (path : string list) (id : string) : global_reference option =
   try
     Some (find_reference "" path id)
   with
   | Anomaly (_,_) -> None
 
+let compare_defs (file : string) (id : string) : bool =
+  let orig = find_reference "Couldn't find original definition in SF file"
+    [file] id in
+  let sub = ofind_reference ["Submission"] id in
+  match sub with
+  | Some sub ->
+    pp (pr_constr (type_of_global orig));
+    pp (brk (0,0));
+    pp (pr_constr (type_of_global orig));
+    pp (brk (0,0));
+    is_conv (type_of_global sub) (type_of_global orig)
+  | None -> false
+
 let () = Mltop.add_known_plugin (fun () ->
   Printf.printf "Grader plugin successfully loaded\n\n";
-  let r = find_reference ["Coq";"Lists";"List"] "app" in
-  match r with
-  | Some r ->
-    pp (pr_global r);
-    pp (brk (0,0));
-    pp (pr_constr (type_of_global r))
-  | None   -> Printf.printf "Couldn't find anything"
+  let result = if compare_defs "MoreCoq" "filter_exercise" then "true" else "false" in
+  Printf.printf "Worked = %s\n\n" result
 ) "grader"
