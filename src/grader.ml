ffad7e5dee2bb68d82fe9d9e7f34eb9b94381f47
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2014-09-30 20:20:11 -0400

    Figure out how to get list of assumptions.

--- src/grader.ml
+++ src/grader.ml
@@ -8,6 +8,13 @@ open Printer
 open Term
 open Util
 
+let (|>) x f = f x
+
+let has_no_assumptions (id : global_reference) : bool =
+  constr_of_global id |>
+  Assumptions.assumptions ~add_opaque:false Names.full_transparent_state |>
+  Assumptions.ContextObjectMap.is_empty
+
 let is_conv (t1 : constr) (t2 : constr) : bool =
   is_conv empty_env Evd.empty t1 t2
 
@@ -27,7 +34,8 @@ let compare_defs (file : string) (id : string) : bool =
     pp (brk (0,0));
     pp (pr_constr (type_of_global orig));
     pp (brk (0,0));
-    is_conv (type_of_global sub) (type_of_global orig)
+    is_conv (type_of_global sub) (type_of_global orig) &&
+    has_no_assumptions sub
   | None -> false
 
 let () = Mltop.add_known_plugin (fun () ->
