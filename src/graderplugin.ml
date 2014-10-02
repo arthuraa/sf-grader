ce1f53620d7ca560b460f87698544a4caa4787d6
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2014-10-02 15:43:48 -0400

    Grade definitions listed on original file.

--- src/graderplugin.ml
+++ src/graderplugin.ml
@@ -8,6 +8,10 @@ open Printer
 open Term
 open Util
 
+type grading_action =
+| GradeAuto of string * int
+| GradeManual of string * int
+
 let sf_path = Sys.getenv "SFGRADERSFPATH"
 let assignment = Sys.getenv "SFGRADERASSIGNMENT"
 let result_file = Sys.getenv "SFGRADERRESULT"
@@ -55,7 +59,7 @@ let find_substring_from (s : string) (i : int) (sub : string) : int =
     else loop (i + 1) in
   loop i
 
-let process_file (path : string) : unit =
+let process_file (path : string) : grading_action list =
   let chan = open_in path in
   let nbytes = in_channel_length chan in
   let s = String.create nbytes in
@@ -63,36 +67,39 @@ let process_file (path : string) : unit =
   let auto_tag = "(* GRADE_THEOREM" in
   let term = "*)" in
   really_input chan s 0 nbytes;
-  let rec loop i =
-    if String.length s <= i then ()
+  let rec loop i acc =
+    if String.length s <= i then acc
     else if occurs_at s i manual_tag then
       let i = i + String.length manual_tag in
       let j = String.index_from s i ':' in
       let k = find_substring_from s i term in
       let n = int_of_string @@ String.trim @@ String.sub s i (j - i) in
       let m = String.trim @@ String.sub s (j + 1) (k - j - 1) in
-      Printf.printf "Found manual %d with message \"%s\"\n" n m;
-      loop (k + String.length term)
+      loop (k + String.length term) (GradeManual (m, n) :: acc)
     else if occurs_at s i auto_tag then
       let i = i + String.length auto_tag in
       let j = String.index_from s i ':' in
       let k = find_substring_from s i term in
       let n = int_of_string @@ String.trim @@ String.sub s i (j - i) in
       let m = String.trim @@ String.sub s (j + 1) (k - j - 1) in
-      Printf.printf "Found auto %d with message \"%s\"\n" n m;
-      loop (k + String.length term)
-    else loop (i + 1) in
+      loop (k + String.length term) (GradeAuto (m, n) :: acc)
+    else loop (i + 1) acc in
   try
-    loop 0;
-    close_in chan
+    let res = loop 0 [] in
+    close_in chan;
+    List.rev res
   with Invalid_argument _ -> Printf.printf "Error while reading file\n"; exit 1
 
 let () =
   Mltop.add_known_plugin (fun () -> ()) "grader";
   Printf.printf "Grader plugin successfully loaded\n\n";
-  (* let out = open_out result in
-     let f   = Format.formatter_of_out_channel out in
-     let result = if compare_defs "MoreCoq" "filter_exercise" then "true" else "false" in
-     Format.fprintf f "Worked = %s@." result;
-     close_out out *)
-  process_file @@ Printf.sprintf "%s/%s.v" sf_path assignment
+  let out = open_out result_file in
+  let f   = Format.formatter_of_out_channel out in
+  let gas = process_file @@ Printf.sprintf "%s/%s.v" sf_path assignment in
+  let do_grade = function
+    | GradeManual (com, n) -> Format.fprintf f "Grade Manually %s %d@." com n
+    | GradeAuto (id, n) ->
+      let result = if compare_defs assignment id then "true" else "false" in
+      Format.fprintf f "Graded Automatically %s %d %s@." id n result in
+  List.iter do_grade gas;
+  close_out out
