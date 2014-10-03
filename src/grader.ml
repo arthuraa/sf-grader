b7616b27e5acdf0ae63339e52f5e3c44a243d2fc
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2014-10-03 15:19:41 -0400

    Option for checking files individually.

--- src/grader.ml
+++ src/grader.ml
@@ -1,12 +1,12 @@
 type options = {
   sf_path : string;
-  submissions : string;
+  submission : string;
   result_file : string
 }
 
 let usage () : 'a =
   List.iter print_endline [
-    "USAGE: grader [OPTION] SUBMISSIONS";
+    "USAGE: grader [OPTION] SUBMISSION";
     "Grade submissions of Software Foundations exercises";
     "";
     "OPTIONS";
@@ -17,7 +17,7 @@ let usage () : 'a =
 
 let read_options () : options =
   let sf_path = ref None in
-  let submissions = ref None in
+  let submission = ref None in
   let result_file = ref None in
   let rec process args =
     begin match args with
@@ -31,22 +31,22 @@ let read_options () : options =
       process args
     | "-o" :: _ -> usage ()
     | path :: args ->
-      if !submissions == None then
-        (submissions := Some path;
+      if !submission == None then
+        (submission := Some path;
          process args)
       else usage ()
     | [] -> ()
     end in
   let args = Array.to_list Sys.argv in
   process (List.tl args);
-  match !sf_path, !submissions with
-  | Some sf_path, Some submissions ->
+  match !sf_path, !submission with
+  | Some sf_path, Some submission ->
     { result_file =
         begin match !result_file with
         | Some rf -> rf
         | None -> "result"
         end;
-      sf_path = sf_path; submissions = submissions }
+      sf_path = sf_path; submission = submission }
   | _, _ -> usage ()
 
 let o = read_options ()
@@ -64,22 +64,50 @@ let ensure_dir_exists dir =
 let (/) base name =
   base ^ "/" ^ name
 
+let noext path =
+  try String.sub path 0 (String.rindex path '.')
+  with
+  | Not_found -> path
+  | Invalid_argument _ -> path
+
+let ext path =
+  try
+    let i = String.rindex path '.' + 1 in
+    String.sub path i (String.length path - i)
+  with
+  | Not_found -> ""
+  | Invalid_argument _ -> ""
+
+let dirname path =
+  try String.sub path 0 (String.rindex path '/')
+  with
+  | Not_found -> "."
+  | Invalid_argument _ -> "."
+
+let basename path =
+  try
+    let i = String.rindex path '/' + 1 in
+    String.sub path i (String.length path - i)
+  with
+  | Not_found -> path
+  | Invalid_argument _ -> path
+
 let cp source dest =
   ignore @@ Sys.command @@ Printf.sprintf "cp %s %s" source dest
 
 let plugin_loader_com = "Declare ML Module \"graderplugin\".\n"
 
-let grade_sub path file : unit =
-  let assignment = String.sub file 0 (String.length file - 2) in
+let grade_sub path : unit =
+  let assignment = noext @@ basename path in
   let tmp = "tmp" / "Submission.v" in
-  cp (path/file) tmp;
-  print_string (path/file);
-  let res = Sys.command @@ Printf.sprintf "coqc -I %s %s > %s.out 2>&1" o.sf_path tmp (path/file) in
+  cp path tmp;
+  print_string path;
+  let res = Sys.command @@ Printf.sprintf "coqc -I %s %s > %s.out 2>&1" o.sf_path tmp path in
   if res <> 0 then
     print_endline " compilation error"
   else begin
     print_endline " ok";
-    let env = Array.append [|"SFGRADERRESULT=" ^ (path/file) ^ ".res";
+    let env = Array.append [|"SFGRADERRESULT=" ^ path ^ ".res";
                              "SFGRADERSFPATH=" ^ o.sf_path;
                              "SFGRADERASSIGNMENT=" ^ assignment |] @@
       Unix.environment () in
@@ -90,23 +118,28 @@ let grade_sub path file : unit =
     let proc = Unix.open_process_full coqcom env in
     let input, output, _ = proc in
     output_string output plugin_loader_com;
-    flush output;
-    match Unix.close_process_full proc with
-    | Unix.WEXITED i -> Printf.printf "WEXITED %u\n" i
-    | _ -> ()
+    flush output
   end
 
-let copy_subs () =
-  let com = Printf.sprintf "unzip -qq %s -d %s" "submissions.zip" "tmp" in
+let grade_subs path =
+  let com = Printf.sprintf "unzip -qq %s -d %s" path "tmp" in
   ignore @@ Sys.command com;
   let files = Sys.readdir "tmp" in
-  ensure_dir_exists "submissions";
+  ensure_dir_exists "submission";
   Array.iter (fun file ->
     let name, file' = translate_file_name file in
-    let dir = "submissions" / name in
+    let dir = "submission" / name in
+    let path = dir / file' in
     ensure_dir_exists dir;
-    cp ("tmp"/file) (dir/file');
-    grade_sub dir file'
+    cp ("tmp"/file) path;
+    grade_sub path
   ) files
 
-let _ = copy_subs ()
+let _ =
+  ensure_dir_exists "tmp";
+  match ext o.submission with
+  | "zip" -> grade_subs o.submission
+  | "v" -> grade_sub o.submission
+  | _ ->
+    Printf.printf "Don't know what to do with file %s\n" o.submission;
+    exit 1
